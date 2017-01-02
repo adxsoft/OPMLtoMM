37f72d8f271615b6646d12825461d7e42cb0211a
adxsoft <adxsoft@gmail.com>
2017-01-02 15:18:47 +1100

    Add files via upload

---
+++ TestData/fromoo3.mm
@@ -0,0 +1,3 @@
+<map version="1.5.9"><node TEXT="fromoo3"><node TEXT="1-1" /><node TEXT="1-2"><node TEXT="2-1"><richcontent TYPE="DETAILS"><html><head /><body><b>BOLD Hello World</b>
+<i>ITALIC Hello World</i>
+</body></html></richcontent></node><node TEXT="2-2" /></node><node TEXT="1-3" /><node TEXT="" /></node></map>
